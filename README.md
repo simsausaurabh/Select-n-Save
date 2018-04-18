b247349f00aa097cb33eab725eac179066d7911a
simsausaurabh <saurabhsri.sri123@gmail.com>
2018-04-18 22:34:48 +0530

    Add AES Encryption for the selected content

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Select-n-Save
-Google chrome extension to select any content on a webpage and save it in a file at local system.
+Google chrome extension to select any content on a webpage and save it as an encrypted data in local sytem.
+
+- The project uses AES encryption algorithm to encrypt the selected content.
+- A secure password is used to encrypt the content and user is free to change the password according to its convenience.
 
 # Format of saved file
 - `<First-word-of-selected-text><Current-Date><space><Current-Time>`
--- saveLocally.js
+++ saveLocally.js
@@ -32,10 +32,14 @@ function save(selectedText) {
 	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 	var dateTime = date+' '+time;
 
+	var myPassword = '123456';
+	var encryptedData = CryptoJS.AES.encrypt(selectedText.selectionText, myPassword);
+	var decrypted = CryptoJS.AES.decrypt(encryptedData, myPassword);
+
 	fileName = fileName+""+dateTime;
 
 	var tempElem = document.createElement('a');
-    tempElem.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(selectedText.selectionText));
+    tempElem.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(encryptedData));
     tempElem.setAttribute('download', fileName);
     tempElem.click();
 }
